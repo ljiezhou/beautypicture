a64f1dfddc495de52190335aa81060f94d70af4f
chinahbzlj <chinahbzlj@163.com>
2018-03-30 17:07:40 +0800

    .

---
+++ app/src/main/java/com/zhou/beautypicture/TT.java
@@ -0,0 +1,21 @@
+package com.zhou.beautypicture;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by 周利杰 on 18-3-30.
+ */
+
+public class TT extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
